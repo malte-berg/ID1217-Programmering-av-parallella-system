7d5ae91179f5cbe130346b5a04fc31696da7a672
Malte Berg <malte@malte.nu>
2025-02-22 11:41:47 +0100

    Remove unnecessary integers used for cyclical buffer

--- HW4/HungryBirds/Monitor_Dish.java
+++ HW4/HungryBirds/Monitor_Dish.java
@@ -1,24 +1,24 @@
 public class Monitor_Dish {
     //Monitor_Dish is a monitor of type Bounded Buffer (lecture 11)
-    private int n, count = 0, front = 0, rear = 0;
+    private int dish_size, worm_count = 0;
 
-    public Monitor_Dish(int n) {
-        this.n = n;
+    public Monitor_Dish(int dish_size) {
+        this.dish_size = dish_size;
     }
     
     public synchronized void add_worm() {
-        while (count == n)
+        while (worm_count == dish_size)
             try {wait();}
             catch (InterruptedException e) { }
-        count++;
+        worm_count++;
         notifyAll();
     }
 
     public synchronized void eat_worm() {
-        while (count == 0)
+        while (worm_count == 0)
             try { wait(); }
             catch (InterruptedException e) { }
-        count--;
+        worm_coun--;
         notifyAll();
     }
 
