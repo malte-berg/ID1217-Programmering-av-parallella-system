2a91c5d16671900c9cac2c1b5389a4251dfe5dc1
Malte Berg <malte@malte.nu>
2025-02-22 14:32:38 +0100

    Add mommy bird class for producer thread

---
+++ HW4/HungryBirds/Mommy_Bird.java
@@ -0,0 +1,14 @@
+public class Mommy_Bird extends Thread {
+    //Producer
+    private Monitor_Dish dish;
+
+    public Mommy_Bird (Monitor_Dish dish) {
+        this.dish = dish;
+    }
+
+    public void run() {
+        while (true) {
+            dish.add_worm();
+        }
+    }
+}
