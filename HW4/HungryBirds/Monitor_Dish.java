989b6ef378d69e54982fc1f9662d4ef82dedcf09
Malte Berg <malte@malte.nu>
2025-02-22 14:19:09 +0100

    Add code for boolean control over chirps

--- HW4/HungryBirds/Monitor_Dish.java
+++ HW4/HungryBirds/Monitor_Dish.java
@@ -1,13 +1,17 @@
 public class Monitor_Dish {
     //Monitor_Dish is a monitor of type shared cell (lecture 12)
     private int dish_size, worm_count = 0;
+    private boolean chirped_empty;
 
     public Monitor_Dish(int dish_size) {
         this.dish_size = dish_size;
+        this.worm_count = dish_size;
+        this.chirped_empty = false;
     }
 
+    // For mommy bird (producer)
     public synchronized void add_worm() {
-        while (worm_count == dish_size) {
+        while (!chirped_empty) {
             try {
                 wait();
             } catch (InterruptedException e) { }
@@ -16,14 +20,19 @@ public class Monitor_Dish {
         notifyAll();
     }
 
+    // For baby birds (consumers)
     public synchronized void eat_worm() {
         while (worm_count == 0) {
+            // If the bird is the first to discover the empty dish, chirp.
+            if (!chirped_empty) {
+                chirped_empty = true;
+                notifyAll();
+            }
             try {
                 wait();
             } catch (InterruptedException e) { }
         }
         worm_count--;
-        notifyAll();
     }
 
 }
