e2df260f9b3f2db93e6543ce6fde99c353745b22
Malte Berg <malte@malte.nu>
2025-02-22 14:34:38 +0100

    Added start of main

--- HW4/HungryBirds/hungrybirds.java
+++ HW4/HungryBirds/hungrybirds.java
@@ -1,3 +1,12 @@
 public class hungrybirds {
-    
+    static int[] worm_tracker;
+    static int no_of_bbirds;
+
+    public static void main(String[] args) {
+        // Set no of baby birds (consumers) to args or 4 if no args.
+        if (args.length > 0) no_of_bbirds = Integer.parseInt(args[0]);
+        else no_of_bbirds = 4;
+
+
+    }
 }
