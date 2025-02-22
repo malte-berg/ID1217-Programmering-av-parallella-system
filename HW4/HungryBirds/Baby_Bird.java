f2355aa0f1c87a5d00cc103e92519d1552859724
Malte Berg <malte@malte.nu>
2025-02-22 15:33:08 +0100

    Implement worm tracking per thread

--- HW4/HungryBirds/Baby_Bird.java
+++ HW4/HungryBirds/Baby_Bird.java
@@ -2,18 +2,21 @@ public class Baby_Bird extends Thread {
     //Consumer
     private Monitor_Dish dish;
     private int id;
+    private int[] worm_tracker;
 
-    public final long SLEEPDURATION = 200;
+    public final long SLEEPDURATION = 0;
 
-    public Baby_Bird (Monitor_Dish dish, int id) {
+    public Baby_Bird (Monitor_Dish dish, int id, int[] worm_tracker) {
         this.dish = dish;
         this.id = id;
+        this.worm_tracker = worm_tracker;
     }
 
     public void run() {
         try {
             while (true) {
                 dish.eat_worm();
+                worm_tracker[id]++;
                 System.out.println("Baby bird " + id + " ate a worm.");
                 Thread.sleep(SLEEPDURATION);
             }
