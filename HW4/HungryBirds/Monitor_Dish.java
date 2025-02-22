2d0a29d9a222bf2e90d9fe28eae87892c64da6d3
Malte Berg <malte@malte.nu>
2025-02-22 11:52:28 +0100

    Fix error in monitor type

--- HW4/HungryBirds/Monitor_Dish.java
+++ HW4/HungryBirds/Monitor_Dish.java
@@ -1,5 +1,5 @@
 public class Monitor_Dish {
-    //Monitor_Dish is a monitor of type Bounded Buffer (lecture 11)
+    //Monitor_Dish is a monitor of type shared cell (lecture 12)
     private int dish_size, worm_count = 0;
 
     public Monitor_Dish(int dish_size) {
