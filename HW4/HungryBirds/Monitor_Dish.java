b347709879ccba2d3b50b2947ed93786ffafc106
Malte Berg <malte@malte.nu>
2025-02-22 14:08:10 +0100

    Fix typo and while-comparison

--- HW4/HungryBirds/Monitor_Dish.java
+++ HW4/HungryBirds/Monitor_Dish.java
@@ -5,24 +5,24 @@ public class Monitor_Dish {
     public Monitor_Dish(int dish_size) {
         this.dish_size = dish_size;
     }
-    
+
     public synchronized void add_worm() {
         while (worm_count == dish_size) {
             try {
                 wait();
             } catch (InterruptedException e) { }
         }
-        worm_count++;
+        worm_count = dish_size;
         notifyAll();
     }
 
     public synchronized void eat_worm() {
         while (worm_count == 0) {
-            try { 
-                wait(); 
+            try {
+                wait();
             } catch (InterruptedException e) { }
         }
-        worm_coun--;
+        worm_count--;
         notifyAll();
     }
 
