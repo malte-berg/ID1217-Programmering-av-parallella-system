f33bae10065719e38ebe919f4a3547d076e46b59
Malte Berg <malte@malte.nu>
2025-02-28 10:55:59 +0100

    Working centralized solution

--- HW5/star.c
+++ HW5/star.c
@@ -17,18 +17,17 @@ int main (int argc, char *argv[]) {
         debug = (strcmp(argv[2], "--DEBUG") == 0);
     }
 
+    int proc_value = rank;
+
     double start_time = MPI_Wtime();
     for (int round = 0; round < NO_OF_ROUNDS; round++) {
         if (rank == 0) {
             int *buf = (int*)malloc(sizeof(int)*size);
 
-            buf[0] = -1;
-
-
             for (int i = 1; i < size; i++) {
                 MPI_Recv(&buf[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 if (debug) {
-                    printf("Collector got value %d from proc %d in round %d.", buf[i], i, round);
+                    printf("Collector got value %d from proc %d in round %d.\n", buf[i], i, round);
                 }
                 buf[i]++; /* Simulated work, adding 1 to value */
             }
@@ -36,14 +35,21 @@ int main (int argc, char *argv[]) {
             for (int i = 1; i < size; i++) {
                 MPI_Send(&buf[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
                 if (debug) {
-                    printf("Collector sent value %d to proc %d in round %d.", buf[i], i, round);
+                    printf("Collector sent value %d to proc %d in round %d.\n", buf[i], i, round);
                 }
             }
         } else {
-
+            MPI_Send(&proc_value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            MPI_Recv(&proc_value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
-        double exec_time = MPI_Wtime() - start_time;
+    }
+    double exec_time = MPI_Wtime() - start_time;
 
+    double max_time;
+    MPI_Reduce(&exec_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        printf("Total runtime: %f\n", max_time);
     }
     MPI_Finalize();
     return 0;
